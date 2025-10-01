3d666cb8de18abfc87f08564812f398b8a395713
Osokoya Fiyinfoluwa <osokoyafiyinfoluwa@gmail.com>
2025-10-01 08:21:02 +0100

    Added background image for each drum

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+var numDrumButtoms = document.querySelectorAll(".drum").length;
+for(i = 0; i<numDrumButtoms; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+    });
+}
